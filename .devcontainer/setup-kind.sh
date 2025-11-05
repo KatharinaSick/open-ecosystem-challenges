517d639f75e5250e525f324c2eef970eff79b9e9
Katharina Sick <katharina.sick@dynatrace.com>
2025-11-05 21:05:33 +0100

    wip

---
+++ .devcontainer/setup-kind.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Installing Kind..."
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.22.0/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+
+echo "Installing kubectl..."
+curl -LO "https://dl.k8s.io/release/$(curl -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+
+echo "Starting Kind cluster..."
+kind create cluster --name codespace --wait 60s
---
+++ README.md
@@ -0,0 +1,11 @@
+[![Static Badge](https://img.shields.io/badge/Open_Ecosystem_Challenge-Quest-FF9800)](https://open-ecosystem.discourse.group/t/about-the-challenges-category/16)
+# Challenge: GitOps Progressive Delivery with Release Telemetry
+
+Challenge story
+
+## What you'll learn
+
+## Time Commitment
+
+## Get Started
+Link to documentation
