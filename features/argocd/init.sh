189a8f5f48a46e922bef3943b1d0004ba3efdfbe
Katharina Sick <katharina.sick@dynatrace.com>
2025-11-12 10:03:27 +0100

    wip

--- features/argocd/init.sh
+++ features/argocd/init.sh
@@ -1,6 +1,63 @@
 #!/usr/bin/env bash
 set -e
 
+showFlags() {
+  echo " --help         Display this help message"
+  echo " --show-flags   Shows available flags"
+  echo " --read-only    Disables the ArgoCD admin user and only provides read-only access"
+}
+
+help() {
+  echo "Usage: $0 [OPTIONS]"
+  echo "Options:"
+  showFlags
+}
+
+# Parse flags
+read_only=false
+
+for arg in "$@"; do
+  case "$arg" in
+    --help)
+      help
+      exit 0
+      ;;
+    --show-flags)
+      showFlags
+      exit 0
+      ;;
+    --read-only)
+      read_only=true
+      ;;
+    *)
+      echo "Unknown option: $arg" >&2
+      exit 1
+      ;;
+  esac
+done
+
 echo "Installing Argo CD..."
 kubectl create namespace argocd
 kubectl apply -k features/argocd/manifests
+
+echo "Installing Argo CD CLI..."
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/download/v3.2.0/argocd-linux-amd64
+sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+rm argocd-linux-amd64
+
+echo "Waiting for Argo CD server to be ready..."
+kubectl wait --namespace argocd --for=condition=available deployment/argocd-server --timeout=300s
+
+echo "Setting password for alice user..."
+admin_password=$(argocd admin initial-password -n argocd)
+argocd login --insecure --username admin --password "$admin_password" --port-forward-namespace argocd
+argocd account update-password \
+  --account alice \
+  --current-password $admin_password \
+  --new-password a-super-secure-password
+
+if [ "$read_only" = true ]; then
+  echo "read only mode enabled"
+fi
+
+
