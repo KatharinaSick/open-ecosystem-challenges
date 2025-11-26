4e42c4f9e81230b1f828934c083afcbb6ddd30c0
Katharina Sick <katharina.sick@dynatrace.com>
2025-11-26 14:13:19 +0100

    wip

--- features/argo-rollouts/init.sh
+++ features/argo-rollouts/init.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "✨ Installing Argo Rollouts"
-# kubectl create namespace argo-rollouts
+kubectl create namespace argo-rollouts
 kubectl apply -n argo-rollouts -f https://github.com/argoproj/argo-rollouts/releases/download/v1.8.3/install.yaml
 
 echo "✨ Waiting for Argo Rollouts controller to be ready"
