5cdcf5c9c005dfcaa472d029c2ac2a0340c0b105
Katharina Sick <katharina.sick@dynatrace.com>
2025-11-26 13:03:21 +0000

    wip

--- features/argo-rollouts/init.sh
+++ features/argo-rollouts/init.sh
@@ -2,11 +2,16 @@
 set -e
 
 echo "✨ Installing Argo Rollouts"
-kubectl create namespace argo-rollouts
-kubectl apply -k features/argo-rollouts/manifests
+# kubectl create namespace argo-rollouts
+kubectl apply -n argo-rollouts -f https://github.com/argoproj/argo-rollouts/releases/download/v1.8.3/install.yaml
+
+echo "✨ Waiting for Argo Rollouts controller to be ready"
+kubectl rollout status deployment/argo-rollouts -n argo-rollouts --timeout=300s
 
 echo "✨ Installing Argo Rollouts Kubectl plugin"
-curl -LO https://github.com/argoproj/argo-rollouts/releases/v1.8.3/download/kubectl-argo-rollouts-linux-amd64
+curl -LO https://github.com/argoproj/argo-rollouts/releases/download/v1.8.3/kubectl-argo-rollouts-linux-amd64
 chmod +x ./kubectl-argo-rollouts-linux-amd64
 sudo mv ./kubectl-argo-rollouts-linux-amd64 /usr/local/bin/kubectl-argo-rollouts
 
+echo "✨ Exposing Argo Rollouts dashboard"
+kubectl argo rollouts dashboard --port 30101 &
