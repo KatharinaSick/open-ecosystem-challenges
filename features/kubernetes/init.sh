70dfc9b015502cc7bace432236aadebfadd5101c
Katharina Sick <katharina.sick@dynatrace.com>
2025-11-27 13:05:30 +0100

    wip

--- features/kubernetes/init.sh
+++ features/kubernetes/init.sh
@@ -17,7 +17,7 @@ curl -sS https://webinstall.dev/k9s@0.50.16 | bash
 
 echo "✨ Installing Helm"
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-4
-chmod 700 get_helm.sh
+chmod 755 get_helm.sh
 ./get_helm.sh
 rm get_helm.sh
 
