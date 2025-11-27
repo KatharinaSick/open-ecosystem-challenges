a830b74591ea5ccbb99abd3446f84fbe93c13135
Katharina Sick <katharina.sick@dynatrace.com>
2025-11-27 09:21:01 +0100

    wip

--- .devcontainer/01-echoes-lost-in-orbit_intermediate/post-start.sh
+++ .devcontainer/01-echoes-lost-in-orbit_intermediate/post-start.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-echo "✨ Starting level 2 - Intermediate"
-
-echo "✨ Exposing Argo Rollouts dashboard"
-kubectl argo rollouts dashboard --port 30101 &
+features/argo-rollouts/connect.sh
 
 #REPO_URL="https://github.com/${GITHUB_REPOSITORY}.git"
 #sed -i "s|__REPO_URL__|${REPO_URL}|g" adventures/01-echoes-lost-in-orbit/beginner/manifests/appset.yaml
---
+++ features/argo-rollouts/connect.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+
+echo "✨ Sleeping 10s to allow previous setups to settle"
+sleep 10s
+
+echo "✨ Exposing Argo Rollouts dashboard"
+kubectl argo rollouts dashboard --port 30101 &
