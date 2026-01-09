e5e9035b96422c1c79bb081174026d7e426ec942
Katharina Sick <katharina.sick@dynatrace.com>
2026-01-09 11:36:15 +0100

    wip: opentofu

--- lib/gcp-api-mock/init.sh
+++ lib/gcp-api-mock/init.sh
@@ -4,4 +4,6 @@ set -e
 echo "✨ Starting the GCP API Mock"
 docker run -d -p 30104:8080 ghcr.io/katharinasick/gcp-api-mock:v1.0.0
 
+export STORAGE_EMULATOR_HOST="http://localhost:30104"
+
 echo "✅ GCP API Mock is ready"
