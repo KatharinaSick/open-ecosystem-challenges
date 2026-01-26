5cf5ade6453783828f258c3f4cbeb699775df23e
Katharina Sick <katharina.sick@dynatrace.com>
2026-01-26 21:51:26 +0100

    wip - expert

--- .devcontainer/02-building-cloudhaven_03-expert/post-start.sh
+++ .devcontainer/02-building-cloudhaven_03-expert/post-start.sh
@@ -28,7 +28,7 @@ git commit -m "chore: configure workflows and Terraform for Codespace" --allow-e
 git push
 
 # Expose GCP mock publicly
-gh codespace ports visibility 30104:public -c $CODESPACE_NAME"
+gh codespace ports visibility 30104:public -c $CODESPACE_NAME
 
 # Create state bucket
 curl -X POST 'http://localhost:30104/storage/v1/b?project=todo' \
