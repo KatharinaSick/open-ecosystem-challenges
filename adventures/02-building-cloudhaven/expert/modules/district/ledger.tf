b083075a7dc2be039ae7a673188588f4dbad1a48
Katharina Sick <katharina.sick@dynatrace.com>
2026-01-27 11:40:33 +0100

    wip - expert

--- adventures/02-building-cloudhaven/expert/modules/district/ledger.tf
+++ adventures/02-building-cloudhaven/expert/modules/district/ledger.tf
@@ -35,7 +35,7 @@ resource "google_sql_database_instance" "ledger" {
     ip_configuration {
       ipv4_enabled    = false
       private_network = "projects/cloudhaven/global/networks/default"
-      require_ssl     = true
+      ssl_mode        = "ENCRYPTED_ONLY"
     }
 
     insights_config {
